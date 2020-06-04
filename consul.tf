e1a1da7c25b68731db939c987de4fdee114c7b38
Max Anderson <maxanderson006@gmail.com>
2020-06-04 15:57:29 -0500

    getting local ip

--- consul.tf
+++ consul.tf
@@ -61,8 +61,6 @@ resource "google_compute_region_instance_group_manager" "consul" {
 data "template_file" "default" {
   template = file("${path.module}/scripts/consul-config.tpl")
   vars = {
-    ip_address =  "1.1.1.1"
-    //ip_address = google_compute_instance_template.consul.network_interface.network_ip
     consul_version = "releases.hashicorp.com/consul/${var.consul_version}/consul_${var.consul_version}_linux_amd64.zip"
   }
 }
