014e80deb41e6455b0d4ee10ded41fc1185b345c
Max Anderson <maxanderson006@gmail.com>
2020-06-04 15:11:46 -0500

    fixing template

--- consul.tf
+++ consul.tf
@@ -61,7 +61,7 @@ resource "google_compute_region_instance_group_manager" "consul" {
 data "template_file" "default" {
   template = file("${path.module}/scripts/consul-config.tpl")
   vars = {
-    ip_address =  1.1.1.1//google_compute_instance_template.consul.network_ip
+    ip_address =  "1.1.1.1" //google_compute_instance_template.consul.network_ip
     consul_version = "releases.hashicorp.com/consul/1.7.3/consul_1.7.3_linux_amd64.zip"
   }
 }
