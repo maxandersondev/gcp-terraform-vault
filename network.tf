71aa002534948a8c3da73dccf67040d9d1ee1113
Max Anderson <maxanderson006@gmail.com>
2020-06-18 16:24:17 -0500

    adding inbound to trust

--- network.tf
+++ network.tf
@@ -61,8 +61,8 @@ resource "google_compute_firewall" "allow-inbound" {
   network = google_compute_network.trust.self_link
   
   allow {
-    protocol = "all"
-    ports    = ["all"]
+    protocol = "tcp"
+    ports    = ["22", "80", "8080", "1000-2000"]
   }
 
   source_ranges = ["10.0.2.0/24"]
